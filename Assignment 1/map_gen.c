091aef29f56e0b5f2a3fdfa1549862a81a0f3b33
Austin Ledesma <ledesma@iastate.edu>
2023-01-31 12:34:08 -0600

    Changed cell types to enum vals and added comments

--- Assignment 1/map_gen.c
+++ Assignment 1/map_gen.c
@@ -4,33 +4,38 @@
 #define SIZE_X 21
 #define SIZE_Y 80
 
+enum cell_type{Mountain = '%', Tree = '^', Road = '#', PokeMart = 'M', PokeCenter = 'C',
+                    Long_Grass = ':', Clearing = '.', PC = '@', Empty = ' '};
+
+// To hold any info a cell may need
 typedef struct cell {
-    char type;
+    enum cell_type type;
 } cell_t;
 
+// Holds a single map
 typedef struct map {
     cell_t map_chars[SIZE_X][SIZE_Y];
     int N_path_start, S_path_start, E_path_start, W_path_start; 
 } map_t;
 
-
+// initializes the starting map with borders
 void init_map(map_t *map){
     int i, j;
     for(i = 0; i < SIZE_X; i++){
         for(j = 0 ; j < SIZE_Y; j++){
-            // cell_t *cell_map = map->map_chars[i][j];
+            // if current cell is on the edge, make it a mountain
+            // Mountain and Empty are enum cell_type
             if(!i || !j || i == SIZE_X - 1 || j == SIZE_Y - 1){
-                // cell_map->type = '%';
-                map->map_chars[i][j].type = '%';
+                map->map_chars[i][j].type = Mountain;
             }
             else{
-                // cell_map->type = ' ';
-                map->map_chars[i][j].type = ' ';
+                map->map_chars[i][j].type = Empty ;
             }
         }
     }
 }
 
+// prints map using enum cell_type values 
 void print_map(map_t *map){
     int i, j;
 
@@ -44,10 +49,9 @@ void print_map(map_t *map){
 
 int main(int argc, char *argv[]){
     map_t map;
-    // printf("%s", map->map_chars[0][0]->type);
+
     init_map(&map);
     print_map(&map);
-    int a = 0;
-    a++;
+    
     return 0;
 }
