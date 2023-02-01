cc4e5810a50e9eba55256fa67c8d1895d1feaea3
Austin Ledesma <ledesma@iastate.edu>
2023-02-01 16:32:00 -0600

    Seeds map with random types following requirements

--- Assignment 1/map_gen.c
+++ Assignment 1/map_gen.c
@@ -1,11 +1,13 @@
 #include <stdio.h> //includes printf
-#include <stdlib.h> // includes malloc
+#include <stdlib.h> // includes malloc, srand
+#include <time.h>
 
+// size of map on terminal
 #define SIZE_X 21
 #define SIZE_Y 80
 
 enum cell_type{Mountain = '%', Tree = '^', Road = '#', PokeMart = 'M', PokeCenter = 'C',
-                    Long_Grass = ':', Clearing = '.', PC = '@', Empty = ' '};
+                    Long_Grass = ':', Clearing = '.', Water = '~', PC = '@', Empty = ' '};
 
 // To hold any info a cell may need
 typedef struct cell {
@@ -18,6 +20,82 @@ typedef struct map {
     int N_path_start, S_path_start, E_path_start, W_path_start; 
 } map_t;
 
+// places random tiles onto the map
+void rand_seed(map_t *map){
+    int i, j, found, rand_type, rand_spot_i, rand_spot_j;
+    enum cell_type seed_list[12] = {Empty, Empty, Empty, Empty, Empty, Empty, 
+                                Empty, Empty, Empty, Empty, Empty, Empty};
+    int next_spot = rand() % 12;
+    int next_type = Long_Grass;
+    
+    // places the required areas in the list
+    for(i = 0; i < 5; i++){
+        found = 1;
+        switch(i){
+            case 0:
+            case 1:
+                next_type = Long_Grass;
+                break;
+            case 2:
+            case 3:
+                next_type = Clearing;
+                break;
+            case 4:
+                next_type = Water;
+        }
+        // keep getting a random array spot until you find an open spot
+        while(found){
+            next_spot = rand() % 12;
+            if(seed_list[next_spot] == Empty){
+                seed_list[next_spot] = next_type;
+                found = 0;
+            }
+        }
+    }
+    
+    // fill in the remaining slots
+    for(i = 0; i < 12; i++){
+        if(seed_list[i] == Empty){
+            rand_type = rand() % 5;
+            switch(rand_type){
+                case 0:
+                    next_type = Mountain;
+                    break;
+                case 1:
+                    next_type = Tree;
+                    break;
+                case 2:
+                    next_type = Clearing;
+                    break;
+                case 3:
+                    next_type = Long_Grass;
+                    break;
+                case 4:
+                    next_type = Water;
+                    break;
+            }
+            seed_list[i] = next_type;
+        }
+    }
+
+    // splits the map into 12 spots and places one in each of those
+    for(i = 0; i < 3; i++){
+        for(j = 0; j < 4; j++){
+            found = 1;
+            // make sure that the random spot isn't on the border
+            while(found){
+                rand_spot_i = (rand() % 7) + (i * 7);
+                rand_spot_j = (rand() % 20) + (j * 20);
+                if(rand_spot_i != 0 && rand_spot_i != 79 && rand_spot_j != 0 && rand_spot_j != 20){
+                    found = 0;
+                }
+            }
+            map->map_chars[rand_spot_i][rand_spot_j].type = seed_list[i + j];
+        }
+    }
+
+}
+
 // initializes the starting map with borders
 void init_map(map_t *map){
     int i, j;
@@ -29,7 +107,7 @@ void init_map(map_t *map){
                 map->map_chars[i][j].type = Mountain;
             }
             else{
-                map->map_chars[i][j].type = Empty ;
+                map->map_chars[i][j].type = Empty;
             }
         }
     }
@@ -49,8 +127,10 @@ void print_map(map_t *map){
 
 int main(int argc, char *argv[]){
     map_t map;
+    srand(time(NULL));
 
     init_map(&map);
+    rand_seed(&map);
     print_map(&map);
     
     return 0;
