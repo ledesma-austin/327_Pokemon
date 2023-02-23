bbca3851abd1290c5f98d91ff1765b4a1ddcfd3a
Austin Ledesma <ledesma@iastate.edu>
2023-02-22 19:51:35 -0600

    Lots of debugging, should work, but doesn't

--- Jeremy_Solutions/poke327-s2023-1.01/poke327.c
+++ Jeremy_Solutions/poke327-s2023-1.01/poke327.c
@@ -45,7 +45,7 @@ typedef uint8_t pair_t[num_dims];
 #define heightxy(x, y) (m->height[y][x])
 
 typedef enum __attribute__ ((__packed__)) terrain_type {
-  ter_debug,
+  // ter_debug,
   ter_boulder,
   ter_tree,
   ter_path,
@@ -55,7 +55,9 @@ typedef enum __attribute__ ((__packed__)) terrain_type {
   ter_clearing,
   ter_mountain,
   ter_forest,
-  ter_water
+  ter_water,
+  ter_gate,
+  num_terrain_types
 } terrain_type_t;
 
 typedef struct map {
@@ -69,6 +71,25 @@ typedef struct queue_node {
   struct queue_node *next;
 } queue_node_t;
 
+typedef enum character_type {
+  char_pc,
+  char_hiker,
+  char_rival,
+  num_character_types
+} character_type_t;
+
+typedef struct cost_map {
+  character_type_t character;
+  int costs[21][80];
+} cost_map_t;
+
+static int move_costs[num_character_types][num_terrain_types] = {
+// Boulder     Tree     Path     Mart   Center    Grass Clearing Mountain   Forest    Water     Gate
+  {INT_MAX, INT_MAX,      10,      10,      10,      20,      10, INT_MAX, INT_MAX, INT_MAX,      10}, // PC
+  {INT_MAX, INT_MAX,      10,      50,      50,      15,      10,      15,      15, INT_MAX, INT_MAX}, // Hiker
+  {INT_MAX, INT_MAX,      10,      50,      50,      20,      10, INT_MAX, INT_MAX, INT_MAX, INT_MAX}  // Rival
+};
+
 static int32_t path_cmp(const void *key, const void *with) {
   return ((path_t *) key)->cost - ((path_t *) with)->cost;
 }
@@ -76,6 +97,130 @@ static int32_t path_cmp(const void *key, const void *with) {
 static int32_t edge_penalty(uint8_t x, uint8_t y)
 {
   return (x == 1 || y == 1 || x == MAP_X - 2 || y == MAP_Y - 2) ? 2 : 1;
+}
+
+static int getCost(terrain_type_t ter, character_type_t char_type){
+  return move_costs[char_type][ter];
+}
+
+static void dijkstra_map_cost(map_t *m, character_type_t char_type, int pc_x, int pc_y, int costs[][MAP_X])
+{
+  static path_t path[MAP_Y][MAP_X], *p;
+  static uint32_t initialized = 0;
+  heap_t h;
+  uint32_t x, y, i, j;
+   
+  if (!initialized) {
+    for (y = 0; y < MAP_Y; y++) {
+      for (x = 0; x < MAP_X; x++) {
+        path[y][x].pos[dim_y] = y;
+        path[y][x].pos[dim_x] = x;
+      }
+    }
+    initialized = 1;
+  }
+  
+  for (y = 0; y < MAP_Y; y++) {
+    for (x = 0; x < MAP_X; x++) {
+      path[y][x].cost = 999999999;
+    }
+  }
+
+  path[pc_x][pc_y].cost = 0;
+
+  heap_init(&h, path_cmp, NULL);
+
+  for (y = 1; y < MAP_Y - 1; y++) {
+    for (x = 1; x < MAP_X - 1; x++) {
+      if(m->map[y][x] != ter_boulder){
+        path[y][x].hn = heap_insert(&h, &path[y][x]);
+      }
+    }
+  }
+
+  while ((p = heap_remove_min(&h))) {
+    p->hn = NULL;
+
+    if ((path[p->pos[dim_y] - 1][p->pos[dim_x]    ].hn) &&
+        (path[p->pos[dim_y] - 1][p->pos[dim_x]    ].cost >
+         (p->cost + getCost(m->map[p->pos[dim_y] - 1]
+                                  [p->pos[dim_x]    ], char_type)))) 
+    { 
+      path[p->pos[dim_y] - 1][p->pos[dim_x]    ].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y] - 1]
+                                  [p->pos[dim_x]    ], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] - 1]
+                                           [p->pos[dim_x]    ].hn);
+    }
+    if ((path[p->pos[dim_y]    ][p->pos[dim_x] - 1].hn) &&
+        (path[p->pos[dim_y]    ][p->pos[dim_x] - 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y]][p->pos[dim_x] - 1], char_type))))) {
+      path[p->pos[dim_y]][p->pos[dim_x] - 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y]]
+                                  [p->pos[dim_x] - 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y]    ]
+                                           [p->pos[dim_x] - 1].hn);
+    }
+    if ((path[p->pos[dim_y]    ][p->pos[dim_x] + 1].hn) &&
+        (path[p->pos[dim_y]    ][p->pos[dim_x] + 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y]][p->pos[dim_x] + 1], char_type))))) {
+      path[p->pos[dim_y]][p->pos[dim_x] + 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y]][p->pos[dim_x] + 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y]    ]
+                                           [p->pos[dim_x] + 1].hn);
+    }
+    if ((path[p->pos[dim_y] + 1][p->pos[dim_x]    ].hn) &&
+        (path[p->pos[dim_y] + 1][p->pos[dim_x]    ].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x]], char_type))))) {
+      path[p->pos[dim_y] + 1][p->pos[dim_x]    ].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x]], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] + 1]
+                                           [p->pos[dim_x]    ].hn);
+    }
+    // y - 1, x - 1
+    if ((path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].hn) &&
+        (path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y]][p->pos[dim_x] - 1], char_type))))) {
+      path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y]][p->pos[dim_x] - 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] - 1]
+                                           [p->pos[dim_x] - 1].hn);
+    }
+    // y + 1, x - 1
+    if ((path[p->pos[dim_y] + 1][p->pos[dim_x] - 1].hn) &&
+        (path[p->pos[dim_y] + 1][p->pos[dim_x] - 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x] - 1], char_type))))) {
+      path[p->pos[dim_y] + 1][p->pos[dim_x] - 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x] - 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] + 1]
+                                           [p->pos[dim_x] - 1].hn);
+    }
+    // y + 1, x + 1
+    if ((path[p->pos[dim_y] + 1][p->pos[dim_x] + 1].hn) &&
+        (path[p->pos[dim_y] + 1][p->pos[dim_x] + 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x] + 1], char_type))))) {
+      path[p->pos[dim_y] + 1][p->pos[dim_x] + 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y] + 1][p->pos[dim_x] + 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] + 1]
+                                           [p->pos[dim_x] + 1].hn);
+    }
+    // y - 1, x - 1
+    if ((path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].hn) &&
+        (path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].cost >
+         ((p->cost + getCost(m->map[p->pos[dim_y] - 1][p->pos[dim_x] - 1], char_type))))) {
+      path[p->pos[dim_y] - 1][p->pos[dim_x] - 1].cost =
+        ((p->cost + getCost(m->map[p->pos[dim_y] - 1][p->pos[dim_x] - 1], char_type)));
+      heap_decrease_key_no_replace(&h, path[p->pos[dim_y] - 1]
+                                           [p->pos[dim_x] - 1].hn);
+    }
+  }
+
+  heap_delete(&h);
+  for(i = 0; i < MAP_Y; i++){
+    for(j = 0; j < MAP_X; j++){
+      costs[i][j] = path[i][j].cost;
+    } 
+  }
 }
 
 static void dijkstra_path(map_t *m, pair_t from, pair_t to)
@@ -689,13 +834,17 @@ static int new_map(map_t *m)
   return 0;
 }
 
-static void print_map(map_t *m)
+static void print_map(map_t *m, int pc_x, int pc_y)
 {
   int x, y;
   int default_reached = 0;
   
   for (y = 0; y < MAP_Y; y++) {
     for (x = 0; x < MAP_X; x++) {
+      if(pc_x == y && pc_y == x){
+        putchar('@');
+      }
+      else{
       switch (m->map[y][x]) {
       case ter_boulder:
       case ter_mountain:
@@ -727,6 +876,7 @@ static void print_map(map_t *m)
         default_reached = 1;
         break;
       }
+      }
     }
     putchar('\n');
   }
@@ -736,11 +886,44 @@ static void print_map(map_t *m)
   }
 }
 
+void print_distance_map(int costs[][MAP_X]){
+  int i, j;
+  
+  for(i = 0; i < MAP_Y; i++){
+    for(j = 0; j < MAP_X; j++){
+      if(costs[i][j] == INT_MAX){
+        printf("   ");
+      }
+      else{
+        printf("%d ", costs[i][j] % 100);
+      }
+    }
+  }
+}
+
+void place_player(map_t* m, int *pc_x, int *pc_y){
+  int x, y;
+  while(1){
+    x = rand() % MAP_Y;
+    y = rand() % MAP_X;
+    if(m->map[x][y] == ter_path){
+      break;
+    }
+  }
+  *pc_x = x;
+  *pc_y = y; 
+}
+
 int main(int argc, char *argv[])
 {
   map_t d;
   struct timeval tv;
   uint32_t seed;
+  int hiker_map[21][80];
+  int rival_map[21][80];
+  character_type_t hiker = char_hiker;
+  character_type_t rival = char_rival;
+  int pc_x, pc_y;
 
   if (argc == 2) {
     seed = atoi(argv[1]);
@@ -753,7 +936,12 @@ int main(int argc, char *argv[])
   srand(seed);
 
   new_map(&d);
-  print_map(&d);
+  place_player(&d, &pc_x, &pc_y);
+  print_map(&d, pc_x, pc_y);
+  dijkstra_map_cost(&d, hiker, pc_x, pc_y, hiker_map);
+  dijkstra_map_cost(&d, rival, pc_x, pc_y, rival_map);
+  print_distance_map(hiker_map);
+  print_distance_map(rival_map);
   
   return 0;
 }
